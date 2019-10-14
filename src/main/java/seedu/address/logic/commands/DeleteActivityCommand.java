bcd8db2bf1d17c04ee54748c8924e5e7ffb6b508
KxxMxxx <kai_1997_ming@hotmail.com>
2019-10-14 21:39:48 +0800

    morph delete command
    implementation of DeleteActivityCommand, DeleteDayCommand not done.

---
+++ src/main/java/seedu/address/logic/commands/DeleteActivityCommand.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Removes an activity from the activity list.
+ */
+public class DeleteActivityCommand extends DeleteCommand {
+    public static final String NEXT_COMMAND_WORD = "activity";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the activity identified by the index number used in the displayed activity list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + DeleteCommand.COMMAND_WORD + " " + NEXT_COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_PERSON_SUCCESS = "Deleted Activity: %1$s";
+
+    private final Index targetIndex;
+
+    public DeleteActivityCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        //to be implemented
+        return new CommandResult(String.format(MESSAGE_DELETE_PERSON_SUCCESS, ""));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteActivityCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteActivityCommand) other).targetIndex)); // state check
+    }
+}
---
+++ src/main/java/seedu/address/logic/commands/DeleteDayCommand.java
@@ -0,0 +1,41 @@
+package seedu.address.logic.commands;
+
+import static java.util.Objects.requireNonNull;
+
+import seedu.address.commons.core.index.Index;
+import seedu.address.logic.commands.exceptions.CommandException;
+import seedu.address.model.Model;
+
+/**
+ * Removes a Day from the itinerary.
+ */
+public class DeleteDayCommand extends DeleteCommand {
+    public static final String NEXT_COMMAND_WORD = "day";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD
+            + ": Deletes the day identified by the index number used in the displayed day list.\n"
+            + "Parameters: INDEX (must be a positive integer)\n"
+            + "Example: " + DeleteCommand.COMMAND_WORD + " " + NEXT_COMMAND_WORD + " 1";
+
+    public static final String MESSAGE_DELETE_DAY_SUCCESS = "Deleted day: %1$d";
+
+    private final Index targetIndex;
+
+    public DeleteDayCommand(Index targetIndex) {
+        this.targetIndex = targetIndex;
+    }
+
+    @Override
+    public CommandResult execute(Model model) throws CommandException {
+        requireNonNull(model);
+        //to be implemented
+        return new CommandResult(String.format(MESSAGE_DELETE_DAY_SUCCESS, targetIndex));
+    }
+
+    @Override
+    public boolean equals(Object other) {
+        return other == this // short circuit if same object
+                || (other instanceof DeleteDayCommand // instanceof handles nulls
+                && targetIndex.equals(((DeleteDayCommand) other).targetIndex)); // state check
+    }
+}
